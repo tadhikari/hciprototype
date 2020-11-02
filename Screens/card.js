acad6a58b6386108a6a00562e00ccc78778c0da7
Tribikram <tribikram.adh01@gmail.com>
2020-11-01 19:06:22 -0700

    bug fixes

--- Screens/card.js
+++ Screens/card.js
@@ -2,7 +2,7 @@
 
 //self.parent.
 var sliderValue = 0;
-var self.parent.currentPage = 'CARD';
+self.parent.currentPage = 'CARD';
 var slider = document.getElementById("cardswipe");
 var input = document.getElementById("pinInput");
 var pay = document.getElementById("next1");
@@ -10,6 +10,30 @@ var cardSwiped = false;
 var pinAdded = false;
 
 
+nextPage = function() {
+
+	
+	if(!cardSwiped){
+		alert(`card swiped`);
+	}
+
+	if(cardSwiped && pinAdded()) {
+
+	}
+
+
+	else {
+
+
+
+
+
+	}
+
+
+}
+
+document.getElementById('next1').addEventListener('click',nextPage);
 
 slider.oninput = function() {
   sliderValue = this.value;
@@ -42,14 +66,14 @@ pinAdded = function() {
 
 	}
 
-	else if(input.value.length < 4){
+	else if(input.value.length < 4 && !isNaN(input.value)){
 
 		alert(`${self.parent.text[self.parent.languageSelected][self.parent.currentPage]['lessthanfour']}`); // pin should be 4 digit long
 		return false;
 	}
 
 	else if(isNaN(input.value)){
-		alert(`not num`);
+		alert(`${self.parent.text[self.parent.languageSelected][self.parent.currentPage]['invalidPin']}`);
 		input.value = '0000'
 		return false;
 	}
@@ -58,37 +82,12 @@ pinAdded = function() {
 	}
 
 	else {
-		alert(`generic error`);
+		alert(`${self.parent.text[self.parent.languageSelected][self.parent.currentPage]['error']}`);
 		return false;
 	}
 
 }
 
 
-
-
-
-nextPage = function() {
-
-	
-	if(!cardSwiped){
-		alert(`card swiped`);
-	}
-
-	if(cardSwiped && pinAdded()) {
-
-	}
-
-
-	else {
-
-
-
-
-
-	}
-
-
-}
 
 
